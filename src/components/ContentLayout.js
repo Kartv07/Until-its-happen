7b7d0bdd4d94cbb0592ec5d4b90ad190f977a160
KartvDHN07 <kartavya@digitalhealthnews.com>
2025-04-26 00:08:00 +0530

    removed maxi width

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar_Layout.js";
 
 export default function ContentLayout({children, sidebarItems}) {
   return (
-    <div className="flex text-white bg-black min-w-screen min-h-screen">
+    <div className="flex text-white bg-black min-h-screen">
       <div className="h-full z-[10] fixed left-0 top-0 border-r-2 border-[#1d1e24]">
         <Sidebar sidebarItems={sidebarItems} />
       </div>
